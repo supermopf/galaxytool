fd8f6de927b21dcb93c800d54543292749f6df34
supermopf <victor.lange1998@gmail.com>
2022-02-10 00:58:15 +0100

    Added CORS

--- secret/firefox.php
+++ secret/firefox.php
@@ -1,6 +1,22 @@
 <?php
 header ("Content-Type:text/xml");
-header ("Access-Control-Allow-Origin: *"); // required for Opera 12 to access the XML response
+header ("Access-Control-Allow-Origin: *"); // CORS
+
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+	exit(0);
+}
+
+
+
 //header("content-type: text/html; charset=utf-8"); // to see what happens even with PHP runtime errors
 
 /*
